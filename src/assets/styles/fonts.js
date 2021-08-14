2527d73d4889bc40bb26688b10ca5af5293cb6ca
Bart van Raaij <bart@linku.nl>
2021-08-14 16:43:31 +0200

    Base layout etc

--- src/assets/styles/fonts.js
+++ src/assets/styles/fonts.js
@@ -1,2 +1,2 @@
-import '@fontsource/source-serif-pro/latin-400.css';
-import '@fontsource/source-sans-pro/latin-400.css';
+// import '@fontsource/source-serif-pro/latin-400.css';
+// import '@fontsource/source-sans-pro/latin-400.css';
